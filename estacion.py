3bb1862843295dbc821d0fe18a0939825a6d5a1e
TsunaHalo2001 <joan.villamil@correounivalle.edu.co>
2025-12-07 19:57:43 -0500

    SCRUM-11 Añadido modulo persistente de comunicacion por wifi

--- estacion.py
+++ estacion.py
@@ -1,28 +1,50 @@
 import dht22
 import machine
+import network
 
 class EstacionMeteorologica:
-    def __init__(self, dht_pin):
-      self.rtc = machine.RTC()
-      self.dht_sensor = dht22.DHT22Sensor(pin_number=dht_pin)
+  def __init__(self, dht_pin):
+    self.rtc = machine.RTC()
+    self.dht_sensor = dht22.DHT22Sensor(pin_number=dht_pin)
 
-      self.mediciones = {
-        'tiempo': self.rtc.datetime(),
-        'temperatura': None,
-        'humedad': None
-       }
+    self.mediciones = {
+      'tiempo': self.rtc.datetime(),
+      'temperatura': None,
+      'humedad': None
+      }
 
-    def obtener_tiempo(self):
-      self.mediciones['tiempo'] = self.rtc.datetime()
+    self.conexiones = [
+      {'SSID' : 'Univalle', 'PASSWORD' : 'Univalle'},
+      {'SSID' : 'VILLAMIL', 'PASSWORD' : 'solylulu12345678'},
+    ]
 
-    def obtener_temperatura(self):
-      self.mediciones['temperatura'] = self.dht_sensor.getTemperature()
+    self.wlan = network.WLAN()
+    self.inicializar_wifi()
 
-    def obtener_humedad(self):
-      self.mediciones['humedad'] = self.dht_sensor.getHumidity()
+  def obtener_tiempo(self):
+    self.mediciones['tiempo'] = self.rtc.datetime()
 
-    def obtener_mediciones(self):
-      self.obtener_tiempo()
-      self.obtener_temperatura()
-      self.obtener_humedad()
-      return self.mediciones
+  def obtener_temperatura(self):
+    self.mediciones['temperatura'] = self.dht_sensor.getTemperature()
+
+  def obtener_humedad(self):
+    self.mediciones['humedad'] = self.dht_sensor.getHumidity()
+
+  def obtener_mediciones(self):
+    self.obtener_tiempo()
+    self.obtener_temperatura()
+    self.obtener_humedad()
+    return self.mediciones
+
+  def inicializar_wifi(self):
+    self.wlan.active(True)
+    if not self.wlan.isconnected():
+      for conexion in self.conexiones:
+        self.wlan.connect(conexion['SSID'], conexion['PASSWORD'])
+        while not self.wlan.isconnected():
+          machine.idle()
+
+        if self.wlan.isconnected():
+          break
+
+    print('Conexión WiFi establecida:', self.wlan.ifconfig())
--- main.py
+++ main.py
@@ -9,11 +9,17 @@ estacion = estacion.EstacionMeteorologica(
 
 def main():
   while True:
+    checkear_conexion()
+
     measurements = estacion.obtener_mediciones()
     time.sleep(1)
     print(measurements['tiempo'])
     print(measurements['temperatura'])
     print(measurements['humedad'])
+
+def checkear_conexion():
+  if not estacion.wlan.isconnected():
+    estacion.inicializar_wifi()
 
 if __name__ == "__main__":
   main()
